a314005e4a1c04f4b688b0840d30d7ce84d13bb5
amaa <amaa@gft.com>
2022-01-06 19:29:12 +0100

    scripts

--- deployment.sh
+++ deployment.sh
@@ -1,28 +1,8 @@
-cd ../fabric-samples/test-network
-./network.sh down
+cd /fabric-samples/test-network
 ./network.sh up
 ./network.sh createChannel
 
-peer lifecycle chaincode package UPMPoly.tar.gz --path ../../UPMPoly/build/install/UPMPoly --lang java --label UPMPoly_1.0
-
-peer lifecycle chaincode install UPMPoly.tar.gz
-
-peer lifecycle chaincode queryinstalled
-
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile /home/vagrant/fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID mychannel --name UPMPoly --version 1.0 --package-id UPMPoly_1.0:a52ebe201af4aa81d68e88bbd44dea3d4d67210611491a2dd36770318e8db71f --sequence 1
-
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name UPMPoly --version 1.0 --sequence 1 --output json
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name UPMPoly --version 1.0 --sequence 1 --output json
-
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile /home/vagrant/fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID mychannel --name UPMPoly --version 1.0 --package-id UPMPoly_1.0:a52ebe201af4aa81d68e88bbd44dea3d4d67210611491a2dd36770318e8db71f --sequence 1
-
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name UPMPoly --version 1.0 --sequence 1 --output json
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name UPMPoly --version 1.0 --sequence 1 --output json
-
-peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile /home/vagrant/fabric-samples/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID mychannel --name UPMPoly --peerAddresses localhost:7051 --tlsRootCertFiles /home/vagrant/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles /home/vagrant/fabric-samples/test-network/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt --version 1.0 --sequence 1
-
-peer lifecycle chaincode querycommitted --channelID mychannel --name UPMPoly
-peer lifecycle chaincode querycommitted --channelID mychannel --name UPMPoly
+./network.sh deployCC -ccn UPMPoly -ccp ../../UPMPoly -ccl java
 
 export FABRIC_CFG_PATH=$PWD/../config/
 
